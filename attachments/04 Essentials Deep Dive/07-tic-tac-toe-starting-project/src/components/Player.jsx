f989ce508e99cb5927658289285ea87f307e1718
joerhew <joerhew@gmail.com>
2024-05-05 13:04:04 -0400

    finished 76

---
+++ attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export default function Player({ name, symbol}) {
+  const [ isEditing, setIsEditing ] = useState(false)
+
+  function handleEditClick() {
+    setIsEditing(!isEditing)
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing &&
+          <span className="player-name">{name}</span>
+        }
+        {!!isEditing &&
+          <input type="text" required value={name} />
+        }
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={ handleEditClick }>
+        {isEditing ? 'Save' : 'Edit'}
+        </button>
+    </li>
+  )
+}
